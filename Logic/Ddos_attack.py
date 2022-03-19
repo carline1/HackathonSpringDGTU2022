bc852b4e2575ae9c404c180eb88e2e9084874250
dobryydenechek <kitte246@gmail.com>
2022-03-19 11:02:18 +0300

    Просто трафик

--- Logic/Ddos_attack.py
+++ Logic/Ddos_attack.py
@@ -3,7 +3,6 @@ import threading
 
 http_list = ['https://ya.ru/', 'https://market.yandex.ru/?clid=505&utm_source=main_stripe_big&src_pof=505&icookie=hVBAmzeIGOxOdyur7QRAOjomWQpIU9KrrVjmeeyM6F3UngD58qJPPKzlecDbwucFtwOjUuMETF3Yf3LcbUiOs%2BtjKUc%3D&utm_source_service=morda', 'https://yandex.ru/news/?utm_source=main_stripe_big', 'https://yandex.ru/images/?utm_source=main_stripe_big']
 
-
 def ddos():
     for http in http_list:
         print(http)
